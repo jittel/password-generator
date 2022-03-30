7833c3a378f6800a24f54abe08e6276e69bf4702
jittel <nathan.c.ginn@gmail.com>
2022-03-29 17:50:57 -0700

    fixed residual undefined on failed attempt

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,48 @@
 function generatePassword() {
-  // todo: list all possible characters
+  // list all possible characters
   var available = "";
+  var result = "";
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var special = "0123456789!#$%&()*+,-./:;<=>?@[\]^_`{|}~';";
-  // todo: ask for parameters (caps, special characters, etc)
+  // ask for parameters (caps, special characters, etc)
   var yesCaps = confirm("we adding caps?");
   var yesSpecial = confirm("we adding special characters?");
-  // todo: show available characters from given params
-  if (yesCaps) {
-    available += uppercase.concat(lowercase);
-  } else if (yesSpecial) {
-    available += special.concat(lowercase);
-  } else if (yesCaps, yesSpecial) {
+  // show available characters from given params
+  if (yesCaps && !yesSpecial) {
+    available += lowercase.concat(uppercase);
+  } else if (!yesCaps && yesSpecial) {
+    available += lowercase.concat(special);
+  } else if (yesCaps && yesSpecial) {
     available += lowercase.concat(uppercase, special);
   } else if (!yesCaps, !yesSpecial) {
-    alert("you need to pick a parameter. please refresh the page");
+    alert("you need to pick a parameter. please try again");
+    return result;
   }
-  // todo: loop through available characters and generate password
-  // todo: return password result
+
+  // checking for proper pw length
+  var length = parseInt(prompt("enter a password length between 8 and 12"));
+  if (length > 12) {
+    alert("password too long, pick a number between 8 and 12")
+  } else if (length < 8) {
+    alert("password too short, pick a number between 8 and 12")
+  } else if (!Number.isInteger(length)) {
+    alert("you need to enter a valid number")
+  }
+
+  // console.log(available)
+  // console.log(length)
+
+  // loop through available characters and generate password
+  for (var i = 0; i < length; i++) {
+    result += available.charAt(Math.floor(Math.random() * available.length))
+  }
+  // return password result
+  if (result === undefined) {
+    console.log("result is empty")
+  } else {
+    return result;
+  }
 }
 
 
